72da186b2d8f5eb6ae7371457435db2088200c25
Victor Fang <victorf588@gmail.com>
2026-04-15 11:59:25 -0400

    Update script.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
+// OLD code
 
+// const addNumbers = (first, second) => {
+//   return first + second;
+// }
+
+// if (typeof module !== 'undefined') {
+//   module.exports = {
+//     addNumbers
+//   }
+// }
+
+
+// NEW code
+document.querySelector('#button')
+  .addEventListener('click', () => {
+    document.querySelector('#output')
+      .textContent = 'world';
+  })
+
+
+// *** Open Terminal in VS Code ***
+// control + shift + `  
+
+// *** Clone repository ***
+// go to a folder via cd
+// git clone repository_link
+
+// git add -A
+// git commit -m "Your commit message"
+// git push
